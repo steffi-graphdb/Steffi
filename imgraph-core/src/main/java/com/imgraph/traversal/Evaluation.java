713827a868ace2d8b4a61fc53424a7931972e228
Nam-Luc Tran <namluc.tran@euranova.eu>
2014-02-13 16:20:06 +0100

    added license information

--- imgraph-core/src/main/java/com/imgraph/traversal/Evaluation.java
+++ imgraph-core/src/main/java/com/imgraph/traversal/Evaluation.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2014 EURA NOVA.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Aldemar Reynaga - initial API and implementation
+ *     Salim Jouili - initial API and implementation
+ ******************************************************************************/
 package com.imgraph.traversal;
 
 import java.util.HashMap;
